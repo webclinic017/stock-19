ed3144ce5d2f0d508667be7002fc3f25a37c7d2a
ks7948_lee <ks7948.lee@samsung.com>
2022-01-18 00:42:30 +0900

    order_target_percent

--- simulator/main.py
+++ simulator/main.py
@@ -21,53 +21,72 @@ class CustomStrategy(bt.Strategy):
         upperband=70,
         lowerband=30,
         # envelope 셋팅
-        envelope_period=14,
-        perc=4
+        envelope_period=30,
+        perc=2.5,
+        # 최대 보유 시간 
+        target_day=5,
+        # 3단 비중 조절
+        first_buy=0.3,
+        second_buy=0.6,
+        third_buy=1.0,
+        extend_due_date=True
     )
 
-
+    def min_date(self) :
+        return datetime.date(1, 1, 1)
+        
     def __init__(self):
         self.holding = 0
         print(self.params.upperband)
         ## RSI_SMA 와 RSI_EMA 차이?
-        self.rsi_sma = bt.ind.RSI_SMA(self.data.close, period=self.params.rsi_period,
+        self.rsi_sma = bt.ind.RSI_EMA(self.data.close, period=self.params.rsi_period,
                                       upperband=self.params.upperband, lowerband=self.params.lowerband, safediv=True)
         self.envelope = bt.ind.MovingAverageSimpleEnvelope(
             self.data.close, perc=self.params.perc, period=self.params.envelope_period)
-        self.signal_started_rsi_10 = False
-        self.signal_started_rsi_20 = False
-        self.signal_started_rsi_30 = False
-        self.signal_due_date = 0
+            
+        self.first_signal = False
+        self.second_signal = False
+        self.third_signal = False
+
+        self.signal_due_date = self.min_date()
 
     def no_signal_started(self) :
-        signal_started = self.signal_started_rsi_30 and self.signal_started_rsi_20 and self.signal_started_rsi_10
+        signal_started = self.first_signal and self.second_signal and self.third_signal
         return signal_started
 
     def next(self):
         # position 이 없을때
-        if self.signal_due_date == 0 and self.rsi_sma < self.params.lowerband and self.envelope.bot < self.data.close :
+        if self.signal_due_date < self.data.datetime.date() and self.rsi_sma < self.params.lowerband and self.envelope.bot > self.data.close :
             # 10일 카운트 시작
-            if self.signal_due_date == 0 :
-                self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=10)
+            if self.signal_due_date == self.min_date() :
+                self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=self.params.target_day)
                 
-            if self.rsi_sma < 30  and self.signal_started_rsi_30 == False:
-                self.order_target_percent(target=0.3)
-                self.signal_started_rsi_30 = True
-            elif self.rsi_sma < 30  and self.signal_started_rsi_20 == False:
-                self.order_target_percent(target=0.6)
-                self.signal_started_rsi_20 = True
-            elif self.rsi_sma < 30  and self.signal_started_rsi_10 == False:
-                self.order_target_percent(target=1.0)
-                self.signal_started_rsi_10 = True
+            if self.rsi_sma < 30  and self.first_signal == False:
+                self.order_target_percent(target=self.params.first_buy)
+                self.first_signal = True
+            elif self.rsi_sma < 30  and self.second_signal == False:
+                self.order_target_percent(target=self.params.second_buy)
+                if self.params.extend_due_date : self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=self.params.target_day)
+                self.second_signal = True
+            elif self.rsi_sma < 30  and self.third_signal == False:
+                self.order_target_percent(target=self.params.third_buy)
+                if self.params.extend_due_date : self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=self.params.target_day)
+                self.third_signal = True
 
         # 이미 position 이 있을때
-        if self.signal_due_date == self.data.datetime :
-            self.signal_due_date = 0
-            self.signal_started_rsi_10 = False
-            self.signal_started_rsi_20 = False
-            self.signal_started_rsi_30 = False
-            self.order_target_percent(target=0.0)
-        
+        elif self.position :
+            if self.signal_due_date == self.min_date() :
+                self.signal_due_date = datetime.min_date()
+                self.third_signal = False
+                self.second_signal = False
+                self.first_signal = False
+                self.order_target_percent(target=0.0)
+            elif self.rsi_sma > 50 : 
+                self.signal_due_date = self.min_date()
+                self.third_signal = False
+                self.second_signal = False
+                self.first_signal = False
+                self.order_target_percent(target=0.0)
 
     def isRedCandle(self):
         if self.data.close[0] > self.data.open[0]:
@@ -100,7 +119,7 @@ if __name__ == "__main__":
 
     # data = bt.feeds.PandasData(dataname=my_data_reader.MyDataReader().get_data_with_time("A052400",20210101,20220101))
 
-    data = bt.feeds.PandasData(dataname=yf.download('^KS11', '2020-01-01', '2021-12-31', auto_adjust=True))
+    data = bt.feeds.PandasData(dataname=yf.download('^KS11', '2021-01-01', '2022-12-31', auto_adjust=True))
 
     cerebro.adddata(data)  # Add the data feed
 
