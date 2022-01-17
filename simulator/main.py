15c207d8a49303cf7a8811437aea29a37bb36df8
ks7948_lee <ks7948.lee@samsung.com>
2022-01-17 23:58:23 +0900

    my_data_reader

--- simulator/main.py
+++ simulator/main.py
@@ -5,6 +5,7 @@ import sqlite3
 import pandas as pd
 import os
 import yfinance as yf
+import my_data_reader
 
 locale.setlocale(locale.LC_ALL, 'ko_KR')
 
@@ -16,14 +17,12 @@ class CustomStrategy(bt.Strategy):
 
     params = dict(
         # rsi 셋팅
-        rsi_period=15,
+        rsi_period=10,
         upperband=70,
         lowerband=30,
         # envelope 셋팅
-        envelope_period=30,
-        fast=2,
-        slow=30,
-        perc=2.5
+        envelope_period=14,
+        perc=4
     )
 
 
@@ -31,33 +30,44 @@ class CustomStrategy(bt.Strategy):
         self.holding = 0
         print(self.params.upperband)
         ## RSI_SMA 와 RSI_EMA 차이?
-        self.rsi_sma = bt.ind.RSI_EMA(self.data.close, period=self.params.rsi_period,
+        self.rsi_sma = bt.ind.RSI_SMA(self.data.close, period=self.params.rsi_period,
                                       upperband=self.params.upperband, lowerband=self.params.lowerband, safediv=True)
-        self.envelope = bt.ind.AdaptiveMovingAverageEnvelope(
-            self.data.close, perc=self.params.perc, period=self.params.envelope_period, fast=self.params.fast, slow=self.params.slow)
+        self.envelope = bt.ind.MovingAverageSimpleEnvelope(
+            self.data.close, perc=self.params.perc, period=self.params.envelope_period)
+        self.signal_started_rsi_10 = False
+        self.signal_started_rsi_20 = False
+        self.signal_started_rsi_30 = False
+        self.signal_due_date = 0
+
+    def no_signal_started(self) :
+        signal_started = self.signal_started_rsi_30 and self.signal_started_rsi_20 and self.signal_started_rsi_10
+        return signal_started
 
     def next(self):
         # position 이 없을때
-        if not self.position:
-            if self.rsi_sma < self.params.lowerband:
-                if self.isRedCandle():
-                    close = self.data.close[0]  # 종가 값
-                    size = int(self.broker.getcash() / close)  # 최대 구매 가능 개수
-                    # 매수 size = 구매 개수 설정
-                    self.buy(size=size-10, price=self.data.open)
-                    self.buy_price = self.data.close
-                    self.from_buy_day = 3
+        if self.signal_due_date == 0 and self.rsi_sma < self.params.lowerband and self.envelope.bot < self.data.close :
+            # 10일 카운트 시작
+            if self.signal_due_date == 0 :
+                self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=10)
+                
+            if self.rsi_sma < 30  and self.signal_started_rsi_30 == False:
+                self.order_target_percent(target=0.3)
+                self.signal_started_rsi_30 = True
+            elif self.rsi_sma < 30  and self.signal_started_rsi_20 == False:
+                self.order_target_percent(target=0.6)
+                self.signal_started_rsi_20 = True
+            elif self.rsi_sma < 30  and self.signal_started_rsi_10 == False:
+                self.order_target_percent(target=1.0)
+                self.signal_started_rsi_10 = True
 
         # 이미 position 이 있을때
-        else:
-            if self.from_buy_day == 0:
-                self.close()
-                self.from_buy_day = 0
-            else:
-                self.from_buy_day = self.from_buy_day - 1
-                if self.data.close < self.buy_price * 0.97:
-                    self.close()
-                    self.from_buy_day = 0
+        if self.signal_due_date == self.data.datetime :
+            self.signal_due_date = 0
+            self.signal_started_rsi_10 = False
+            self.signal_started_rsi_20 = False
+            self.signal_started_rsi_30 = False
+            self.order_target_percent(target=0.0)
+        
 
     def isRedCandle(self):
         if self.data.close[0] > self.data.open[0]:
@@ -83,41 +93,14 @@ class CustomStrategy(bt.Strategy):
         print('%s : %s[%d]  holding[%d] price[%d]   cash[%.2f]  value[%.2f]'
               % (date, action, abs(order.size), self.holding, stock_price, cash, value))
 
-
-class MyDataReader:
-    def __init__(self):
-        print(os.getcwd())
-        self.datapath = ('../Creon-Datareader-master/db/일봉/일봉_전종목.db')
-        if os.path.exists(self.datapath):
-            print("DB file is OK! ("+self.datapath+")")
-        else:
-            print(self.datapath + " in not exist!")
-
-    # code = "D0011025" # 코나아이
-    # code = "A052400 # 코나아이
-    # code = "A005930" # 삼성전자
-    # code = "A306200" # 세아제강
-    def get_data_with_time(self, code="A052400", start_date=20191201, last_date=20220101):
-        con = sqlite3.connect(self.datapath)
-        dataframe = pd.read_sql('SELECT * FROM ' + code + ' WHERE date BETWEEN ' +
-                                str(start_date) + ' AND ' + str(last_date), con)
-        dataframe['date'] = pd.to_datetime(
-            dataframe['date'], format='%Y%m%d', errors='raise')
-        dataframe.rename(columns={'date': 'datetime'}, inplace=True)
-        # dataframe.columns.values[0] = 'datetime'
-        dataframe.set_index('datetime', inplace=True)
-        return dataframe
-
-
 if __name__ == "__main__":
     cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
     cerebro.broker.setcash(100000000)
     cerebro.broker.setcommission(0.003)
 
-    # data = bt.feeds.PandasData(dataname=MyDataReader().get_data_with_time("A052400",20210101,20220101))
+    # data = bt.feeds.PandasData(dataname=my_data_reader.MyDataReader().get_data_with_time("A052400",20210101,20220101))
 
-    data = bt.feeds.PandasData(dataname=yf.download(
-        '^KS11', '2021-01-01', '2021-12-31', auto_adjust=True))
+    data = bt.feeds.PandasData(dataname=yf.download('^KS11', '2020-01-01', '2021-12-31', auto_adjust=True))
 
     cerebro.adddata(data)  # Add the data feed
 
---
+++ simulator/my_data_reader.py
@@ -0,0 +1,27 @@
+import sqlite3
+import pandas as pd
+import os
+
+class MyDataReader:
+    def __init__(self):
+        print(os.getcwd())
+        self.datapath = ('../Creon-Datareader-master/db/일봉/일봉_전종목.db')
+        if os.path.exists(self.datapath):
+            print("DB file is OK! ("+self.datapath+")")
+        else:
+            print(self.datapath + " in not exist!")
+
+    # code = "D0011025" # 코나아이
+    # code = "A052400 # 코나아이
+    # code = "A005930" # 삼성전자
+    # code = "A306200" # 세아제강
+    def get_data_with_time(self, code="A052400", start_date=20201201, last_date=20220101):
+        con = sqlite3.connect(self.datapath)
+        dataframe = pd.read_sql('SELECT * FROM ' + code + ' WHERE date BETWEEN ' +
+                                str(start_date) + ' AND ' + str(last_date), con)
+        dataframe['date'] = pd.to_datetime(
+            dataframe['date'], format='%Y%m%d', errors='raise')
+        dataframe.rename(columns={'date': 'datetime'}, inplace=True)
+        # dataframe.columns.values[0] = 'datetime'
+        dataframe.set_index('datetime', inplace=True)
+        return dataframe
