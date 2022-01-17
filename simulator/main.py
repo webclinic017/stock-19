402bf2890afabf4db449bbb44c0d045645363490
ks7948_lee <ks7948.lee@samsung.com>
2022-01-18 02:02:51 +0900

    rsi & envelope

--- simulator/main.py
+++ simulator/main.py
@@ -25,18 +25,19 @@ class CustomStrategy(bt.Strategy):
         perc=2.5,
         # 최대 보유 시간 
         target_day=5,
+        extend_due_date=True,
         # 3단 비중 조절
+        rsi_first_band=30,
+        rsi_second_band=20,
+        rsi_third_band=10,
         first_buy=0.3,
         second_buy=0.6,
-        third_buy=1.0,
-        extend_due_date=True
+        third_buy=0.95,
     )
 
-    def min_date(self) :
-        return datetime.date(1, 1, 1)
-        
     def __init__(self):
         self.holding = 0
+        self.sum_price=0
         print(self.params.upperband)
         ## RSI_SMA 와 RSI_EMA 차이?
         self.rsi_sma = bt.ind.RSI_EMA(self.data.close, period=self.params.rsi_period,
@@ -50,6 +51,9 @@ class CustomStrategy(bt.Strategy):
 
         self.signal_due_date = self.min_date()
 
+    def min_date(self) :
+        return datetime.date(1, 1, 1)
+        
     def no_signal_started(self) :
         signal_started = self.first_signal and self.second_signal and self.third_signal
         return signal_started
@@ -60,15 +64,14 @@ class CustomStrategy(bt.Strategy):
             # 10일 카운트 시작
             if self.signal_due_date == self.min_date() :
                 self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=self.params.target_day)
-                
-            if self.rsi_sma < 30  and self.first_signal == False:
+            if self.rsi_sma < self.params.rsi_first_band  and self.first_signal == False:
                 self.order_target_percent(target=self.params.first_buy)
                 self.first_signal = True
-            elif self.rsi_sma < 30  and self.second_signal == False:
+            elif self.rsi_sma < self.params.rsi_second_band  and self.second_signal == False:
                 self.order_target_percent(target=self.params.second_buy)
                 if self.params.extend_due_date : self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=self.params.target_day)
                 self.second_signal = True
-            elif self.rsi_sma < 30  and self.third_signal == False:
+            elif self.rsi_sma < self.params.rsi_third_band  and self.third_signal == False:
                 self.order_target_percent(target=self.params.third_buy)
                 if self.params.extend_due_date : self.signal_due_date = self.data.datetime.date() + datetime.timedelta(days=self.params.target_day)
                 self.third_signal = True
@@ -99,18 +102,26 @@ class CustomStrategy(bt.Strategy):
             return
 
         if order.isbuy():
-            action = 'Buy'
+            action = 'Buy :'
+            sum_factor = 1
         elif order.issell():
-            action = 'Sell'
+            action = 'Sell:'
+            sum_factor = -1
 
         date = self.data.datetime.date()
         stock_price = self.data.close[0]
         cash = self.broker.getcash()
         value = self.broker.getvalue()
         self.holding += order.size
-
-        print('%s : %s[%d]  holding[%d] price[%d]   cash[%.2f]  value[%.2f]'
-              % (date, action, abs(order.size), self.holding, stock_price, cash, value))
+        self.sum_price += order.size * stock_price * sum_factor
+        avg_price = 0
+        if self.holding != 0 : 
+            avg_price = self.sum_price / self.holding
+        else : 
+            self.sum_price = 0
+            
+        print('%s : %s  가격[%d]    주문수량[%d]  평단가[%d]  보유주식[%d]  현금보유[%.0f]  평가잔액[%.0f]'
+              % (date, action, stock_price, abs(order.size), avg_price, self.holding, cash, value))
 
 if __name__ == "__main__":
     cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
@@ -118,10 +129,13 @@ if __name__ == "__main__":
     cerebro.broker.setcommission(0.003)
 
     # data = bt.feeds.PandasData(dataname=my_data_reader.MyDataReader().get_data_with_time("A052400",20210101,20220101))
+    # 000660.KS 하이닉스 
+    # ^KS11 코스피
+    kospi = bt.feeds.PandasData(dataname=yf.download('^KS11', '2021-01-01', '2022-12-31', auto_adjust=True))
+    data = bt.feeds.PandasData(dataname=yf.download('000660.KS', '2021-01-01', '2022-12-31', auto_adjust=True))
 
-    data = bt.feeds.PandasData(dataname=yf.download('^KS11', '2021-01-01', '2022-12-31', auto_adjust=True))
-
-    cerebro.adddata(data)  # Add the data feed
+    cerebro.adddata(kospi)  # Add the data feed
+    cerebro.adddata(data)
 
     cerebro.addstrategy(CustomStrategy)  # Add the trading strategy
 
@@ -131,7 +145,7 @@ if __name__ == "__main__":
 
     print('* 시작 평가잔액 : %s won' %
           locale.format_string('%d', start_value, grouping=True))
-    print('* 종가 평가잔액 : %s won' %
+    print('* 종료 평가잔액 : %s won' %
           locale.format_string('%d', final_value, grouping=True))
     print('*    수익율     : %.2f %%' %
           ((final_value - start_value) / start_value * 100.0))
