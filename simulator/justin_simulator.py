3d46ee1e3599dace73e6657004a03d27cbb053eb
ks7948_lee <ks7948.lee@samsung.com>
2022-01-21 00:16:35 +0900

    simulator

--- simulator/justin_simulator.py
+++ simulator/justin_simulator.py
@@ -2,24 +2,68 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5 import QtGui
 from PyQt5 import uic
 
+import backtrader as bt
+import yfinance as yf
+
+import numpy as np
 import pandas as pd
+
+import multiprocessing as mp
 from pandas_to_pyqt_table import PandasModel
+from simulator_core import Simulator
+
+from multiprocessing.dummy import Pool as ThreadPool 
+import time
+import traceback
+import sys
 
 # .ui 파일에서 직접 클래스 생성하는 경우 주석 해제
 Ui_MainWindow = uic.loadUiType("justin_simulator.ui")[0]
 
+
+class Worker(QThread):
+    finished = pyqtSignal(dict)
+
+    def __init__(self, codes_dataframe, cash, commission, start_date, last_date, plot, index_data, core_num = 8):
+        super().__init__()
+        self.core_num = core_num
+        self.filter_list = codes_dataframe
+        self.index_data = index_data
+        self.plot = plot
+        self.cash = cash
+        self.commission = commission
+        self.start_date = start_date
+        self.last_date = last_date
+
+    def run(self):
+        pool = ThreadPool(self.core_num) 
+        results = pool.map(self.calculate_yield, self.filter_list['종목코드'])
+        print(results)
+        # self.finished.emit(data)
+
+    def calculate_yield(self, code):
+        _yield = Simulator(cash=self.cash, commission=self.commission).simulate_each(code=code,
+            index_data=self.index_data, start_date=self.start_date, last_date=self.last_date, plot=False)
+        return _yield
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)  
-        # self.kosdaq = False
-        # self.kospi = False
-        self.checkbox_filter_kosdaq.stateChanged.connect(self.chage_filter_kosdaq_status)
-        self.checkbox_filter_kospi.stateChanged.connect(self.chage_filter_kospi_status)
+        self.setupUi(self)
+        self.index = '^KS11'  # 코스피
+        self.radioButton_kosdaq.clicked.connect(
+            self.chage_filter_market_status)
+        self.radioButton_kospi.clicked.connect(self.chage_filter_market_status)
         self.pushButton_filter.clicked.connect(self.filter_start)
+        self.pushButton_start_simulation.clicked.connect(self.simulation_start)
+        self.disable_all_children(self.groupBox_buy_condition)
+        self.disable_all_children(self.groupBox_sell_condition)
+        self.disable_all_children(self.groupBox_simulation)
+
         self.center()
 
     def center(self):
@@ -28,45 +72,103 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    def chage_filter_kosdaq_status(self,state) :
-        self.kosdaq = True if state ==2 else self.kosdaq == False
-        print("self.kosdaq =" + str(self.kosdaq))
-        
-    def chage_filter_kospi_status(self,state) :
-        self.kospi = True if state ==2 else self.kospi == False
-        print("self.kospi = " + str(self.kospi))
-    
-    def filter_start(self) : 
-        self.pushButton_filter.setEnabled(False)
-        
+    def chage_filter_market_status(self, state):
+        if self.radioButton_kosdaq.isChecked():
+            print("radioButton_kosdaq checked!")
+        elif self.radioButton_kospi.isChecked():
+            print("radioButton_kospi checked!")
+
+    def travel_all_children(self, groupbox, bool):
+        for child in groupbox.findChildren(QLineEdit):
+            child.setEnabled(bool)
+        for child in groupbox.findChildren(QComboBox):
+            child.setEnabled(bool)
+        for child in groupbox.findChildren(QRadioButton):
+            child.setEnabled(bool)
+        for child in groupbox.findChildren(QPushButton):
+            child.setEnabled(bool)
+
+    def disable_all_children(self, groupbox):
+        self.travel_all_children(groupbox, False)
+
+    def enable_all_children(self, groupbox):
+        self.travel_all_children(groupbox, True)
+
+    def simulation_start(self):
+        self.disable_all_children(self.groupBox_buy_condition)
+        self.disable_all_children(self.groupBox_sell_condition)
+        self.disable_all_children(self.groupBox_simulation)
+        self.disable_all_children(self.groupBox_filter)
+        print('시뮬레이션 start')
+
+        if self.radioButton_kosdaq.isChecked():
+            self.index = '^KQ11'  # 코스닥
+        elif self.radioButton_kospi.isChecked():
+            self.index = '^KS11'  # 코스피
+        self.cash = int(self.lineEdit_start_cash.text())
+        self.commission = float(self.lineEdit_commission.text())
+
+        # simulate_each(self, code="000660.KS", index='^KQ11', start_date='2018-01-01', last_date='2018-12-31', plot=True):
+        self.start_date = self.comboBox_start_year.currentText(
+        ) + "-" + self.comboBox_start_month.currentText() + "-01"
+        self.last_date = self.comboBox_last_year.currentText(
+        ) + "-" + self.comboBox_last_month.currentText() + "-31"
+
+        self.index_data = bt.feeds.PandasData(dataname=yf.download(
+            tickers=self.index, start_date=self.start_date, last_date=self.last_date, auto_adjust=True, progress=False))
+
+        # self.tableWidget.setRowCount(len(self.filter_list))
+
+        self.worker = Worker(codes_dataframe = self.filter_list, commission=self.commission, cash=self.cash,
+                             index_data=self.index_data, start_date=self.start_date, last_date=self.last_date, core_num = 1, plot=False)
+        self.worker.start()
+
+    # def simulate_each(self, code):
+
+    #     # called by each thread
+    #     def get_web_data(url):
+    #         return {'col1': 'something', 'request_data': requests.get(url).text}
+
+    #     urls = ["http://google.com", "http://yahoo.com"]
+    #     results = pool.map(get_web_data, urls)
+
+    def filter_start(self):
+        self.enable_all_children(self.groupBox_buy_condition)
+        self.enable_all_children(self.groupBox_sell_condition)
+        self.enable_all_children(self.groupBox_simulation)
+        self.disable_all_children(self.groupBox_filter)
+
         dict = {}
-        if self.checkbox_filter_kosdaq.isChecked() is True:
-            with open('kosdaq150.list','r') as file:
-                for line in file: 
+        if self.radioButton_kosdaq.isChecked() is True:
+            with open('kosdaq150.list', 'r') as file:
+                for line in file:
                     code = line.strip()
                     dict[code] = "코스닥150"
-        if self.checkbox_filter_kospi.isChecked() is True:
-            with open('kospi200.list','r') as file:
-                for line in file : 
+        elif self.radioButton_kospi.isChecked() is True:
+            with open('kospi200.list', 'r') as file:
+                for line in file:
                     code = line.strip()
                     dict[code] = "코스피200"
 
-        filter_list = pd.DataFrame(list(dict.items()),
-                                       columns=('종목코드', '사장구분'))
+        self.filter_list = pd.DataFrame(list(dict.items()),
+                                        columns=('종목코드', '사장구분'))
 
-        self.db_view_model = PandasModel(filter_list)
+        self.db_view_model = PandasModel(self.filter_list)
         self.tableView.setModel(self.db_view_model)
         self.tableView.resizeColumnToContents(0)
 
+
 app = QApplication
+
 
 def main_gui():
     global app
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
-    
+
     mainWindow.show()
     app.exec_()
+
 
 if __name__ == "__main__":
     main_gui()
--- simulator/simulator_core.py
+++ simulator/simulator_core.py
@@ -4,11 +4,14 @@ import locale
 import sqlite3
 import pandas as pd
 import os
+from pymysql import NULL
 import yfinance as yf
 import my_data_reader
 import argparse
 
 locale.setlocale(locale.LC_ALL, 'ko_KR')
+
+DEBUG = True
 
 class CustomStrategy(bt.Strategy):
     # list of parameters which are configurable for the strategy
@@ -21,64 +24,66 @@ class CustomStrategy(bt.Strategy):
         # envelope 셋팅
         envelope_period=30,
         perc=2.5,
-        # 최대 보유 시간 
+        # 최대 보유 시간
         target_day=10,
         target_due_date=True,
         # 익절 조건
-        target_rsi = 70,
+        target_rsi=70,
         # 3단 비중 조절
         rsi_first_band=30,
         rsi_second_band=20,
         rsi_third_band=10,
         first_buy=0.3,
         second_buy=0.6,
-        third_buy=0.95, 
+        third_buy=0.95,
     )
 
     def __init__(self):
         self.holding = 0
-        self.sum_price=0
+        self.sum_price = 0
         print(self.params.upperband)
         ## RSI_SMA 와 RSI_EMA 차이?
         self.rsi_sma = bt.ind.RSI_EMA(self.data.close, period=self.params.rsi_period,
                                       upperband=self.params.upperband, lowerband=self.params.lowerband, safediv=True)
         self.envelope = bt.ind.MovingAverageSimpleEnvelope(
             self.data.close, perc=self.params.perc, period=self.params.envelope_period)
-            
+
         self.first_signal = False
         self.second_signal = False
         self.third_signal = False
 
         self.signal_due_date = 0
 
-    def min_date(self) :
-        return datetime.date(1, 1, 1)
-        
-    def signal_started(self) :
+    def signal_started(self):
         signal_started = self.first_signal or self.second_signal or self.third_signal
         return signal_started
 
     def next(self):
         # position 이 없을때
-        if self.rsi_sma < self.params.lowerband and self.envelope.bot > self.data.close :
+        if self.rsi_sma < self.params.lowerband and self.envelope.bot > self.data.close:
             # 10일 카운트 시작
             if self.signal_due_date == 0:
                 self.signal_due_date = self.params.target_day
             # RSI 조건별로 주문
-            if self.rsi_sma < self.params.rsi_first_band  and self.first_signal == False:
-                self.order_target_percent(target=self.params.first_buy, data=self.datas[1])
+            if self.rsi_sma < self.params.rsi_first_band and self.first_signal == False:
+                self.order_target_percent(
+                    target=self.params.first_buy, data=self.datas[1])
                 self.first_signal = True
-            elif self.rsi_sma < self.params.rsi_second_band  and self.second_signal == False:
-                self.order_target_percent(target=self.params.second_buy, data=self.datas[1])
-                if self.params.target_due_date : self.signal_due_date = self.params.target_day
+            elif self.rsi_sma < self.params.rsi_second_band and self.second_signal == False:
+                self.order_target_percent(
+                    target=self.params.second_buy, data=self.datas[1])
+                if self.params.target_due_date:
+                    self.signal_due_date = self.params.target_day
                 self.second_signal = True
-            elif self.rsi_sma < self.params.rsi_third_band  and self.third_signal == False:
-                self.order_target_percent(target=self.params.third_buy, data=self.datas[1])
-                if self.params.target_due_date : self.signal_due_date = self.params.target_day
+            elif self.rsi_sma < self.params.rsi_third_band and self.third_signal == False:
+                self.order_target_percent(
+                    target=self.params.third_buy, data=self.datas[1])
+                if self.params.target_due_date:
+                    self.signal_due_date = self.params.target_day
                 self.third_signal = True
 
         # 이미 position 이 있을때
-        elif self.signal_started() :
+        elif self.signal_started():
             self.signal_due_date -= 1
             if self.signal_due_date <= 0:
                 self.signal_due_date = 0
@@ -86,18 +91,12 @@ class CustomStrategy(bt.Strategy):
                 self.second_signal = False
                 self.first_signal = False
                 self.order_target_percent(target=0.0, data=self.datas[1])
-            elif self.rsi_sma > self.params.target_rsi : 
+            elif self.rsi_sma > self.params.target_rsi:
                 self.signal_due_date = 0
                 self.third_signal = False
                 self.second_signal = False
                 self.first_signal = False
-                self.order_target_percent(target=0.0,data=self.datas[1])
-
-    def isRedCandle(self):
-        if self.data.close[0] > self.data.open[0]:
-            return True
-        else:
-            return False
+                self.order_target_percent(target=0.0, data=self.datas[1])
 
     def notify_order(self, order):
         if order.status not in [order.Completed]:
@@ -116,44 +115,70 @@ class CustomStrategy(bt.Strategy):
         value = self.broker.getvalue()
         self.holding += order.size
         self.sum_price += order.size * stock_price * sum_factor
-        avg_price = 0 
-        if self.holding != 0 : 
+        avg_price = 0
+        if self.holding != 0:
             avg_price = self.sum_price / self.holding
-        else : 
+        else:
             self.sum_price = 0
-            
-        print('%s : %s  가격[%d]    주문수량[%d]  평단가[%d]  보유주식[%d]  현금보유[%.0f]  평가잔액[%.0f]'
-              % (date, action, stock_price, abs(order.size), avg_price, self.holding, cash, value))
+
+        if DEBUG:
+            print('%s : %s  가격[%d]    주문수량[%d]  평단가[%d]  보유주식[%d]  현금보유[%.0f]  평가잔액[%.0f]'
+                  % (date, action, stock_price, abs(order.size), avg_price, self.holding, cash, value))
+
+
+class Simulator:
+
+    def __init__(self, cash=100000000, commission=0.3):
+        # self.cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+        self.cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+        self.cerebro.broker.setcash(cash)
+        self.cerebro.broker.setcommission(commission/100)
+
+    def simulate_each(self, code="000660.KS", index_data=NULL, index='^KQ11', start_date='2018-01-01', last_date='2018-12-31', plot=True):
+        # data = bt.feeds.PandasData(dataname=my_data_reader.MyDataReader().get_data_with_time("A052400",20210101,20220101))
+
+        # code = "000660.KS"  # 하이닉스
+        # code = "005930.KS" # 삼성전자
+        # code = "306200.KS" # 세아제강
+        # code = "207940.KS" # 삼성바이오로직스
+        # code = "052400.KQ" # 코나아이
+
+        # index = '^KS11' # 코스피
+        # index = '^KQ11'  # 코스닥
+        # start_date = '2018-01-01'
+        # last_date = '2018-12-31'
+
+        if index_data is NULL:
+            index_data = bt.feeds.PandasData(dataname=yf.download(
+                index, start_date, last_date, auto_adjust=True,progress = False))
+        data = bt.feeds.PandasData(dataname=yf.download(
+            tickers = code, start = start_date, end = last_date, auto_adjust=True,progress = False))
+
+        self.cerebro.adddata(index_data)  # Add the data feed
+        self.cerebro.adddata(data)
+
+        self.cerebro.addstrategy(CustomStrategy)  # Add the trading strategy
+
+        start_value = self.cerebro.broker.getvalue()
+        self.cerebro.run()  # run it all
+        final_value = self.cerebro.broker.getvalue()
+        if DEBUG:
+            print(code)
+        if DEBUG:
+            print('* 시작 평가잔액 : %s won' %
+                  locale.format_string('%d', start_value, grouping=True))
+        if DEBUG:
+            print('* 종료 평가잔액 : %s won' %
+                  locale.format_string('%d', final_value, grouping=True))
+        
+        _yeild = (final_value - start_value) / start_value * 100.0
+        print('*    수익율     : %.2f %%' %
+              (_yeild))
+
+        # and plot it with a single command
+        if plot == True:
+            self.cerebro.plot(style='candle', barup='red', bardown='blue')
+        return _yeild
 
 if __name__ == "__main__":
-    cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
-    cerebro.broker.setcash(100000000)
-    cerebro.broker.setcommission(0.003)
-
-    # data = bt.feeds.PandasData(dataname=my_data_reader.MyDataReader().get_data_with_time("A052400",20210101,20220101))
-    code = "000660.KS" # 하이닉스 
-    # code = "052400.KQ" # 코나아이
-    # code = "005930.KS" # 삼성전자
-    # code = "306200.KS" # 세아제강
-    # code = "207940.KS" # 삼성바이오로직스
-    # ^KS11 코스피
-    start_date =  '2018-01-01'
-    last_date =  '2018-12-31'
-    kospi = bt.feeds.PandasData(dataname=yf.download('^KS11',start_date,last_date, auto_adjust=True))
-    data = bt.feeds.PandasData(dataname=yf.download(code, start_date, last_date, auto_adjust=True))
-
-    cerebro.adddata(kospi)  # Add the data feed
-    cerebro.adddata(data)
-
-    cerebro.addstrategy(CustomStrategy)  # Add the trading strategy
-
-    start_value = cerebro.broker.getvalue()
-    cerebro.run()  # run it all
-    final_value = cerebro.broker.getvalue()
-
-    print('* 시작 평가잔액 : %s won' % locale.format_string('%d', start_value, grouping=True))
-    print('* 종료 평가잔액 : %s won' % locale.format_string('%d', final_value, grouping=True))
-    print('*    수익율     : %.2f %%' % ((final_value - start_value) / start_value * 100.0))
- 
-    # and plot it with a single command
-    cerebro.plot(style='candle', barup='red', bardown='blue')
+    Simulator().simulate_each()
